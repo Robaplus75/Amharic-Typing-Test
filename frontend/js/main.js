dafa9d47d4882f2790929bfc8c28f707e8d7e0be
robel <robelwon75@gmail.com>
2024-05-10 17:18:03 +0300

    remade the js and added input bar for the words

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,111 +1,125 @@
-const textElement = document.getElementById('text');
-cursorElement="<div>|</div>";
-correctOpen = '<span style="color:white">'
-wrongOpen = '<span style="color:red">'
-colorClose = "</span>"
+const input_bar = document.getElementById('input_bar');
+const text = document.getElementById('text');
 
-let { text, cursorPos } = getTextAndCursorPosition(document.getElementById('text').innerText);
-let totalLetters = text.length;
-let cursorPosition = totalLetters;
-let letterPosition = 0;
+let amh_text = "ፈጣኑ ቡናማ ቀበሮ ከግድግዳው በላይ ዘለለ.";
+let eng_text = "The quick brown fox jumps over the wall.";
+let amh_btn = document.getElementById("amh_btn");
+let eng_btn = document.getElementById("eng_btn")
 
+let words_list = text.innerText.split(" ");
+let sentence_length = words_list.length;
+let word_count = 0;
+let word_mistakes = 0;
 
-document.addEventListener('keypress', (e) => {
-  const keyPressed = e.key;
-  console.log(`${e.key} key was pressed`)
-  if (cursorPosition <= 1){
-    alert("The End")
-    const loading = document.getElementById("loading-circle");
-    const body_content = document.getElementById("body_content");
+input_bar.value="";
 
-    body_content.style.display = "none";
-    loading.style.display = "flex"
-    location.reload(true);
-  }
+highlighted_word = "<span class='highlighter'>"+words_list[word_count]+"</span>";
+before_highlight = text.innerHTML;
+after_highlight = textEdit(text.innerHTML, word_count-sentence_length, highlighted_word);
+text.innerHTML = after_highlight;
 
-  if (keyPressed === text[letterPosition]) {
-    console.log("===============================================================")
-    console.log(`Letter Position: ${letterPosition}`)
-    console.log(`${keyPressed} matches with the ${text[totalLetters - cursorPosition]}`)
-    insertCursor(textElement, cursorElement, cursorPosition);
-    changeColor(textElement, "correct", cursorPosition)
+input_bar.addEventListener("input", (key)=>{
+    letter = key.data;
+    console.log("logger triggered")
+    console.log(`${letter} is pressed`)
 
-    cursorPosition--;
-    letterPosition++;
-    //console.log(text);
-    // Update the text element
-    // textElement.innerText = text;
-  }
-  else {
-    console.log("===============================================================")
-    console.log(`Letter Position: ${letterPosition}`)
-    console.log(`${keyPressed} Does Not matche with the ${text[totalLetters - cursorPosition]}`)
-    // Insert the cursor
-    insertCursor(textElement, cursorElement, cursorPosition);
-    changeColor(textElement, "wrong", cursorPosition)
+    if (letter === " "){
+      text.innerHTML = before_highlight;
 
-    cursorPosition--;
-    letterPosition++;
-  }
-});
+      let word = input_bar.value.slice(0, -1);
+      if (words_list[word_count] == word){
+        console.log(`${word} is correct`);
+        changed_color = '<span style="color:white">'+words_list[word_count]+'</span>';
+        console.log(`Text: ${text.innerHTML}`);
+        console.log(`Word_count: ${word_count}`)
+        let result = textEdit(text.innerHTML, word_count-sentence_length, changed_color);
+        console.log(`Result: ${result}`);
+        
+        text.innerHTML = result;
+        word_count += 1;
+        input_bar.value="";
 
+        highlighted_word = "<span class='highlighter'>"+words_list[word_count]+"</span>";
+        before_highlight = text.innerHTML;
+        after_highlight = textEdit(text.innerHTML, word_count-sentence_length, highlighted_word);
+        text.innerHTML = after_highlight;
+      }
+      else {
+        console.log(` ${word} is not the same as ${words_list[word_count]}`);
+        changed_color = '<span style="color:red">'+words_list[word_count]+'</span>';
+        let result = textEdit(text.innerHTML, word_count-sentence_length, changed_color);
+        console.log(result);
+        text.innerHTML = result;
+        word_count += 1;
+        word_mistakes += 1;
+        input_bar.value="";
 
-function getTextAndCursorPosition(initialText) {
-  return { text: initialText, cursorPosition: 0 };
-}
-
-function insertCursor(textElement, cursor, index) {
-    let innertext = textElement.innerHTML;
-    console.log(textElement.innerHTML)
-    console.log(`text before: ${innertext}`)
-    console.log(`element to be removed: '${cursorElement}'`)
-    innertext = innertext.replace(cursorElement, '');
-    //innertext = innertext.replace(correctOpen, '');
-    //innertext = innertext.replace(colorClose, '');
-    
-    console.log(`text after: ${innertext}`)
-    console.log(`cursorPosition: ${index}`)
-    textElement.innerHTML = insertAtIndexBackward(innertext, cursor, -(index-1))
-    console.log(`text final: ${textElement.innerHTML}`)
-}
-
-function changeColor(textElement, stat, cursorPosition){
-    
-        console.log("Correct: color change process begin")
-        let innertext = textElement.innerHTML;
-        console.log(`color before: ${innertext}`)
-        if (stat == "correct"){
-            innertext = insertAtIndexBackward(innertext, correctOpen, -(cursorPosition+cursorElement.length));
-            console.log(`color after <span>: ${innertext}`)
-            innertext = insertAtIndexBackward(innertext, colorClose, -(cursorPosition+cursorElement.length-1));
-            console.log(`color after: ${innertext}`)
-        }else{
-            innertext = insertAtIndexBackward(innertext, wrongOpen, -(cursorPosition+cursorElement.length));
-            console.log(`color after <span>: ${innertext}`)
-            innertext = insertAtIndexBackward(innertext, colorClose, -(cursorPosition+cursorElement.length-1));
-            console.log(`color after: ${innertext}`)
-        }
-        textElement.innerHTML = innertext;
-    // else {
-    //     console.log("Wrong: color change process begin")
-    //     let innertext = textElement.innerHTML;
-    //     console.log(`color before: ${innertext}`)
-    //     innertext = insertAtIndexBackward(innertext, wrongOpen, -(cursorPosition+cursorElement.length));
-    //     console.log(`color after <span>: ${innertext}`)
-    //     innertext = insertAtIndexBackward(innertext, colorClose, -(cursorPosition+cursorElement.length-1));
-    //     console.log(`color after: ${innertext}`)
-
-    //     textElement.innerHTML = innertext;
-    //  }
-}
-
-function insertAtIndex(str, insert, index) {
-    return str.substr(0, index) + insert + str.substr(index);
-  }
-
-function insertAtIndexBackward(str, insert, index) {
-    if (index < 0) {
-      index = str.length + index;
+        highlighted_word = "<span class='highlighter'>"+words_list[word_count]+"</span>";
+        before_highlight = text.innerHTML;
+        after_highlight = textEdit(text.innerHTML, word_count-sentence_length, highlighted_word);
+        text.innerHTML = after_highlight;
+      }
     }
-    return str.substr(0, index) + insert + str.substr(index);
+    if (word_count >= sentence_length){
+      alert(`The End, You have made ${word_mistakes} mistakes`)
+      const loading = document.getElementById("loading-circle");
+      const body_content = document.getElementById("body_content");
+
+      body_content.style.display = "none";
+      loading.style.display = "flex"
+      location.reload(true);
+    }
+    
   }
+)
+
+function textEdit(sentence, index, word) {
+  const words = sentence.split(' ');
+  if (index >= -words.length && index < words.length) {
+    if (index < 0) {
+      index += words.length;
+    }
+    words[index] = word;
+    return words.join(' ');
+  } else {
+    return sentence;
+  }
+}
+function amh_lang(){
+  text.innerText = amh_text;
+  amh_btn.style.color = "var(--color1)";
+  eng_btn.style.color = "var(--color2)";
+
+  words_list = text.innerText.split(" ");
+  sentence_length = words_list.length;
+  word_count = 0;
+  word_mistakes = 0;
+
+  input_bar.value="";
+
+  highlighted_word = "<span class='highlighter'>"+words_list[word_count]+"</span>";
+  before_highlight = text.innerHTML;
+  after_highlight = textEdit(text.innerHTML, word_count-sentence_length, highlighted_word);
+  text.innerHTML = after_highlight;
+
+  input_bar.focus();
+}
+function eng_lang(){
+  text.innerText = eng_text;
+  amh_btn.style.color = "var(--color2)";
+  eng_btn.style.color = "var(--color1)";
+
+  words_list = text.innerText.split(" ");
+  sentence_length = words_list.length;
+  word_count = 0;
+  word_mistakes = 0;
+
+  input_bar.value="";
+
+  highlighted_word = "<span class='highlighter'>"+words_list[word_count]+"</span>";
+  before_highlight = text.innerHTML;
+  after_highlight = textEdit(text.innerHTML, word_count-sentence_length, highlighted_word);
+  text.innerHTML = after_highlight;
+
+  input_bar.focus();
+}
